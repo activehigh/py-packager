3e68a65bee8bafe87d23aae06144e8caac8a35dc
Moody <mahmudul.islam@rokt.com>
2020-04-21 14:17:31 +1000

    Test commit

--- py_packager/file_generator.py
+++ py_packager/file_generator.py
@@ -1,32 +1,28 @@
 
 from .collectors import *
 from datetime import datetime
+from importlib_resources import files, read_text
+# Reads contents with UTF-8 encoding and returns str.
 import json
 import os
 
 class FileGenerator():
     def generate_manifest(self):
         with open("MANIFEST.in", "w") as f:
-            # write first part
-            with open('./data/MANIFEST.in') as s:
-                f.writelines(s.readlines())  
-                s.close()   
+            content = files('py_packager').joinpath('data/MANIFEST.in').read_text()            
+            f.writelines(content) 
             f.close()
 
     def generate_readme(self):
-        with open("README.rst", "w") as f:
-            # write first part
-            with open('./data/README.rst') as s:
-                f.writelines(s.readlines())  
-                s.close()   
+        with open("README.rst", "w") as f:            
+            content = files('py_packager').joinpath('data/README.rst').read_text()            
+            f.writelines(content) 
             f.close()
 
     def generate_requirements(self):
         with open("requirements.txt", "w") as f:
-            # write first part
-            with open('./data/requirements.txt') as s:
-                f.writelines(s.readlines())  
-                s.close()   
+            content = files('py_packager').joinpath('data/requirements.txt').read_text()            
+            f.writelines(content) 
             f.close()
 
     def generate_folders(self, prompts):
@@ -44,11 +40,10 @@ class FileGenerator():
             os.makedirs("./docs")
 
     def generate_setup(self, prompts: list):
-        with open("setup-test.py", "w") as f:
+        with open("setup.py", "w") as f:
             # write first part
-            with open('./data/setup') as s:
-                f.writelines(s.readlines())  
-                s.close()   
+            content = files('py_packager').joinpath('data/setup').read_text()            
+            f.writelines(content)  
 
             package_details = {}
             for x in prompts:
@@ -58,11 +53,9 @@ class FileGenerator():
             
             f.write(json.dumps(package_details, sort_keys=True, indent=4))
 
-            # write 2nd part
-            with open('./data/setup_2') as s:
-                f.writelines(s.readlines())
-                s.close()
-                
+            # write 2nd part            
+            content = files('py_packager').joinpath('data/setup_2').read_text()            
+            f.writelines(content)                  
             f.close()
         
 
@@ -73,13 +66,9 @@ class FileGenerator():
                 name = x.answer
 
         with open('LICENSE', 'w') as f:
-            with open("./data/LICENSE", "r") as s:
-                lines = s.readlines()
-                for line in lines:
-                    formatted = line.replace('[YEAR]', str(datetime.now().year))
-                    formatted = formatted.replace('[AUTHOR]', name)
-                    f.writelines(formatted)
-                s.close()
+            content = files('py_packager').joinpath('data/LICENSE').read_text()
+            formatted = content.replace('[YEAR]', str(datetime.now().year)).replace('[AUTHOR]', name)
+            f.writelines(formatted)
             f.close()
 
 
